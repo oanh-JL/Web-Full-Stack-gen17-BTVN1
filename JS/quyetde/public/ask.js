006bbe102ca8edf5f4af5ebc01e963a5a6c88f37
OanhPham <oanhpham99x@gmail.com>
2018-12-28 11:42:07 +0700

    BTVN6

--- JS/quyetde/public/ask.js
+++ JS/quyetde/public/ask.js
@@ -1,6 +1,9 @@
 
-document.getElementById("content").addEventListener("input", function() {
-    document.getElementById("count").innerText = 200 - document.getElementById("content").value.length;
-    var check = 200 - document.getElementById("content").value.length;
-    console.log(check);
+// document.getElementById("content").addEventListener("input", function() {
+//     document.getElementById("count").innerText = 200 - document.getElementById("content").value.length;
+//     var check = 200 - document.getElementById("content").value.length;
+//     console.log(check);
+// });
+$("#content").on("input",function(){
+ $("#count").text(200-$("#content").val().length);
 });
--- JS/quyetde/server.js
+++ JS/quyetde/server.js
@@ -8,16 +8,37 @@ app.get("/",(req,res)=>{
     const questions=JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"}));
     if(questions.length==0) res.send("chưa có câu hỏi nào!!!")
     else{
-        var randomQuestion =questions[Math.floor(Math.random()*questions.length)]
-        res.send(`<h1> ${randomQuestion.content} </h1>
-            <form method="POST" action="vote/${randomQuestion.id}">
-                <button type="submit" name="vote" value="y">Đúng/Có/Phải</button>
-                <button type="submit" name="vote" value="n">Sai/Không/Trái</button>
-            </form>` 
-        ); 
-      
+        const  randomQuestion =questions[Math.floor(Math.random()*questions.length)]
+        // res.send(`<h1> ${randomQuestion.content} </h1>
+          
+        //     <a href="/vote/${randomQuestion.id}/yes"> <button>Đúng/Có/Phải</button>
+        //     </a>
+        //     <a href="/vote/${randomQuestion.id}/no"> <button>Sai/Không/Trái</button>
+        //     </a>`
+        // ); 
+      res.sendFile(__dirname+"/view/answer.html");
     }
     
+});
+
+app.get("/api/random",(req,res)=>{
+    const questions=JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"}));
+    const randomQuestion=questions[Math.floor(Math.random()*questions.length)];
+    res.send({question:randomQuestion});
+});
+app.get("/vote/:questionId/:vote",(req,res)=>{
+    const questionId=req.params.questionId;
+    const vote=req.params.vote;
+    const questions=JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"}));
+    questions.forEach((question,index)=>{
+        if(question.id==questionId){
+            // if(vote==yes) questions[index].yes+=1;
+            // else questions[index].no+=1;
+            questions[index][vote]+=1;
+        }
+    });
+    fs.writeFileSync("./question.json",JSON.stringify(questions));
+    res.redirect("/");
 });
 //do ask.html muon sd js trong folder public  sd methord dem so ky tu nen phai tao 1 con router sd static cho car folder
 app.use(express.static("public"));
@@ -26,28 +47,30 @@ app.get("/ask",(req,res)=>{
     res.sendFile(__dirname+"/view/ask.html");
 
 });
+app.get("/resolve",(req,res)=>{
+    res.send(__dirname+"/question")
+})
 
-
-app.post("/vote/:id",(req,res,next)=>{
-    var question=JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"}));
-    for(let i=0;i<question.length;i++){
-      if(question[i].id==req.param.id){
-        var flagQuestion=question[i];
-         console.log(flagQuestion);
-          if(req.body.vote=="y"){
-              flagQuestion.yes++;
-          }
-          if(req.body.vote=="n"){
-              flagQuestion.no++;
-          }
-          console.log(flagQuestion);
-          break;
-      }
+// app.post("/vote/:id",(req,res,next)=>{
+//     var question=JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"}));
+//     for(let i=0;i<question.length;i++){
+//       if(question[i].id==req.param.id){
+//         var flagQuestion=question[i];
+//          console.log(flagQuestion);
+//           if(req.body.vote=="y"){
+//               flagQuestion.yes++;
+//           }
+//           if(req.body.vote=="n"){
+//               flagQuestion.no++;
+//           }
+//           console.log(flagQuestion);
+//           break;
+//       }
       
-      fs.writeFileSync("./question.json",JSON.stringify(question));
-      res.redirect("/");
-    }
-});
+//       fs.writeFileSync("./question.json",JSON.stringify(question));
+//       res.redirect("/");
+//     }
+// });
 
 app.post("/addquestion",(req,res)=>{
     //cach pho thong lay du lieu ma hoa roi 
